0d004565cd05d41443d9987f378facaa88233cc0
GameOfThreads2018 <51769551+GameOfThreads2018@users.noreply.github.com>
2020-05-07 17:00:45 -0400

    Add shadows (#6)
    * add_shadows intial commit
    
    * add_shadows refractor code

--- Finalraytracer/Tracer.cpp
+++ Finalraytracer/Tracer.cpp
@@ -43,30 +43,30 @@ int main()
 			float u = (float)(l + (r - l) * (x + .5F) / 512);
 			float v = (float)(b + (t - b) * (y + .5F) / 512);
 			// Setup scene 3 spheres and one plane
-			Sphere redSphere = Sphere(Point3D(-4.0F, 0.0F, -7.0F), 1.0F);
-			Sphere greenSphere = Sphere(Point3D(0.0F, 0.0F, -7.0F), 2.0F);
-			Sphere blueSphere = Sphere(Point3D(4.0F, 0.0F, -7.0F), 1.0F);
+			Sphere red_sphere = Sphere(Point3D(-4.0F, 0.0F, -7.0F), 1.0F);
+			Sphere green_sphere = Sphere(Point3D(0.0F, 0.0F, -7.0F), 2.0F);
+			Sphere blue_sphere = Sphere(Point3D(4.0F, 0.0F, -7.0F), 1.0F);
 			Plane plane = Plane(Point3D(0.0F, -2.0F, 0.0F), Normal(0.0F, -2.0F, 0.0F));
 
-			Ray viewingRay = Ray(Point3D(), Vector3D(u, v, -.1F));
-			Vector3D result = shade(viewingRay, redSphere, greenSphere, blueSphere, plane);
+			Ray viewing_ray = Ray(Point3D(), Vector3D(u, v, -.1F));
+			Vector3D result = shade(viewing_ray, red_sphere, green_sphere, blue_sphere, plane);
 			outfile << result.x * 255.0F << " " << result.y * 255.0F << " " << result.z * 255.0F << "\n";
 		}
 	}
 }
 
-	Vector3D shade(Ray &ray, Sphere& redSphere, Sphere& greenSphere, Sphere& blueSphere, Plane& plane)
+	Vector3D shade(Ray &ray, Sphere& red_sphere, Sphere& green_sphere, Sphere& blue_sphere, Plane& plane)
 	{
 		Vector3D Ls = Vector3D();
 		Vector3D Ld = Vector3D();
 		
-		if (redSphere.hit(ray))
+		if (red_sphere.hit(ray))
 		{
 			Vector3D ka = Vector3D(0.2F, 0.0F, 0.0F);
 			Vector3D kd = Vector3D(1.0F, 0.0F, 0.0F);
 			Vector3D ks = Vector3D(0.0F, 0.0F, 0.0F);
-			Vector3D hit_point = redSphere.calculateInersectPoint(ray);
-			Vector3D surface_normal = redSphere.calculateNormal(ray);
+			Vector3D hit_point = red_sphere.calculateInersectPoint(ray);
+			Vector3D surface_normal = red_sphere.calculateNormal(ray);
 			Light light = Light(Point3D(), Vector3D(-4.0F, 4.0F, -3.0F) - hit_point);
 			light.normalize();
 			surface_normal.normalize();
@@ -81,14 +81,14 @@ int main()
 		}
 		
 		
-		else if (greenSphere.hit(ray))
+		else if (green_sphere.hit(ray))
 		{
 			Vector3D ka = Vector3D(0.0F, 0.2F, 0.0F);
 			Vector3D kd = Vector3D(0.0F, 0.5F, 0.0F);
 			Vector3D ks = Vector3D(0.5F, 0.5F, .5F);
 
-			Vector3D hit_point = greenSphere.calculateInersectPoint(ray);
-			Vector3D surface_normal = greenSphere.calculateNormal(ray);
+			Vector3D hit_point = green_sphere.calculateInersectPoint(ray);
+			Vector3D surface_normal = green_sphere.calculateNormal(ray);
 			Light light = Light(Point3D(), Vector3D(-4.0F, 4.0F, -3.0F) - hit_point);
 			light.normalize();
 			surface_normal.normalize();
@@ -102,14 +102,14 @@ int main()
 			return L;
 		}
 		
-		else if (blueSphere.hit(ray))
+		else if (blue_sphere.hit(ray))
 		{
 			Vector3D ka = Vector3D(0.0F, 0.0F, 0.2F);
 			Vector3D kd = Vector3D(0.0F, 0.0F, 1.0F);
 			Vector3D ks = Vector3D(0.0F, 0.0F, 0.0F);
 		
-			Vector3D hit_point = blueSphere.calculateInersectPoint(ray);
-			Vector3D surface_normal = blueSphere.calculateNormal(ray);
+			Vector3D hit_point = blue_sphere.calculateInersectPoint(ray);
+			Vector3D surface_normal = blue_sphere.calculateNormal(ray);
 			Light light = Light(Point3D(), Vector3D(-4.0F, 4.0F, -3.0F) - hit_point);
 			light.normalize();
 			surface_normal.normalize();
@@ -118,6 +118,11 @@ int main()
 			Vector3D h = ray.getDirection() - light.direction;
 			h.normalize();
 			Ls = ks * powf(surface_normal.dotProduct(h), 0);
+			Ray shadow_ray = Ray(Point3D(hit_point.x, hit_point.y, hit_point.z), light.direction);
+			if (green_sphere.hit(shadow_ray))
+			{
+				return ka;
+			}
 			Vector3D L = Ld + Ls + ka;
 			ceiling(L);
 			return L;
@@ -134,11 +139,26 @@ int main()
 			Light light = Light(Point3D(), Vector3D(-4.0F, 4.0F, -3.0F) - hit_point);
 			light.normalize();
 			surface_normal.normalize();
+			Ray shadow_ray = Ray(Point3D(hit_point.x, hit_point.y, hit_point.z), light.direction);
 			Ld = kd * fmax(0.0F, surface_normal.dotProduct(light.direction));
 			ray.normalize();
 			Vector3D h = ray.getDirection() - light.direction;
 			h.normalize();
 			Ls = ks * powf(surface_normal.dotProduct(h), 0);
+
+			if (blue_sphere.hit(shadow_ray))
+			{
+				return ka;
+			}
+			else if (red_sphere.hit(shadow_ray))
+			{
+				return ka;
+			}
+			else if (green_sphere.hit(shadow_ray))
+			{
+				return ka;
+			}
+
 			Vector3D L = Ld + Ls + ka;
 			ceiling(L);
 			return L;
